02b63b97722e376cdaac3e69b086eca9002d01c0
Prashant yadav <prashantyadav.p86@gmail.com>
2024-03-19 17:56:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 4000;
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Welcome</h1>");
+});
+app.listen(port, () => {
+  console.log("Linstening on port " + port);
+});
